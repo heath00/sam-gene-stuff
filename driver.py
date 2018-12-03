e6484fdf38d65c6a77e6b99fb275e64410eeb648
heath00 <HeathReineke2018@u.northwestern.edu>
2018-12-03 16:40:16 -0600

    fixed instructions up a bit

--- driver.py
+++ driver.py
@@ -1,6 +1,6 @@
 from gene_file import gene_files
 from gene_stats import gene, subgene
-inp = input("Enter run filenames separated by a comma (i.e. run 1.csv, run 2.csv): ")
+inp = input("Enter run filenames separated by a comma (i.e. run 1.csv,run 2.csv): ")
 strt = input("Enter first gene to be considered (0 indexed, so if you want to skip the first you do 1): ")
 strt = int(strt)
 end = input("Enter the final gene to be considered (the number of the final gene in the spreadsheet like 17 or whatever): ")
